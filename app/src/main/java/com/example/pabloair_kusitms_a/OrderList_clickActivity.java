568901a590436761508a32eaecdc49e71cc4fae7
MinseoShindor <tlsalstj01@duksung.ac.kr>
2022-09-18 15:08:56 +0900

    20220918:ListView

---
+++ app/src/main/java/com/example/pabloair_kusitms_a/OrderList_clickActivity.java
@@ -0,0 +1,14 @@
+package com.example.pabloair_kusitms_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderList_clickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_list_click);
+    }
+}
