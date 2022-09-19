c807b245d726a08a431bca824b3d23bf4c9c4a0b
yoonjeongin <yoonjeongin@naver.com>
2022-09-19 11:18:20 +0900

    [FEAT] Order + Order detail with QR code button

--- app/src/main/java/com/example/pabloair_kusitms_a/Main_Order_Activity.java
+++ app/src/main/java/com/example/pabloair_kusitms_a/Main_Order_Activity.java
@@ -1,6 +1,10 @@
 package com.example.pabloair_kusitms_a;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,20 @@ public class Main_Order_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_order);
+
+        Button OrderBtn = findViewById(R.id.order_submit_btn);
+        View.OnClickListener BtnEvent = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == OrderBtn) {
+                    Intent intent = new Intent(getApplication(), Main_Order_Detail_Activity.class);
+                    startActivity(intent);
+                    Log.d("Activity", "OrderDetailActivity");
+                }
+            }
+        };
+        OrderBtn.setOnClickListener(BtnEvent);
+
+
     }
 }
